ffe70137896b5732cb10851c8d048c84f538355a
rajatsaha18 <editorrajatdiu@gmail.com>
2023-03-16 15:01:04 +0600

    First Commit

---
+++ src/components/includes/MyFooter.vue
@@ -0,0 +1,116 @@
+<template>
+    <div class="my-footer">
+        <div class="newsletter">
+            <div class="container">
+                <div class="w3agile_newsletter_left">
+                    <h3>sign up for our newsletter</h3>
+                </div>
+                <div class="w3agile_newsletter_right">
+                    <form action="#" method="post">
+                        <input type="email" name="Email" required="">
+                        <input type="submit" value="subscribe now">
+                    </form>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+        <!-- //newsletter -->
+        <div style="margin: 8px auto; display: block; text-align:center;">
+
+            <!---728x90--->
+
+        </div>
+        <div class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-sm-6 w3_footer_grid">
+                        <h3>information</h3>
+                        <ul class="w3_footer_grid_list">
+                            <li><a href="events.html"><i class="fa fa-angle-right mr-2" aria-hidden="true"></i> Events</a>
+                            </li>
+                            <li><a href="about.html"><i class="fa fa-angle-right mr-2" aria-hidden="true"></i> About Us</a>
+                            </li>
+                            <li><a href="products.html"><i class="fa fa-angle-right mr-2" aria-hidden="true"></i> Best
+                                    Deals</a></li>
+                            <li><a href="services.html"><i class="fa fa-angle-right mr-2" aria-hidden="true"></i>
+                                    Services</a></li>
+                            <li><a href="short-codes.html"><i class="fa fa-angle-right mr-2" aria-hidden="true"></i> Short
+                                    Codes</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 w3_footer_grid">
+                        <h3>policy info</h3>
+                        <ul class="w3_footer_grid_list">
+                            <li><a href="faqs.html"><i class="fa fa-angle-right mr-2" aria-hidden="true"></i> FAQ</a></li>
+                            <li><a href="privacy.html"><i class="fa fa-angle-right mr-2" aria-hidden="true"></i> privacy
+                                    policy</a></li>
+                            <li><a href="privacy.html"><i class="fa fa-angle-right mr-2" aria-hidden="true"></i> terms of
+                                    use</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 w3_footer_grid">
+                        <h3>what in stores</h3>
+                        <ul class="w3_footer_grid_list">
+                            <li><a href="pet.html"><i class="fa fa-angle-right mr-2" aria-hidden="true"></i> Pet Food</a>
+                            </li>
+                            <li><a href="frozen.html"><i class="fa fa-angle-right mr-2" aria-hidden="true"></i> Frozen
+                                    Snacks</a></li>
+                            <li><a href="kitchen.html"><i class="fa fa-angle-right mr-2" aria-hidden="true"></i> Kitchen</a>
+                            </li>
+                            <li><a href="products.html"><i class="fa fa-angle-right mr-2" aria-hidden="true"></i> Branded
+                                    Foods</a></li>
+                            <li><a href="household.html"><i class="fa fa-angle-right mr-2" aria-hidden="true"></i>
+                                    Households</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 w3_footer_grid">
+                        <h3>twitter posts</h3>
+                        <ul class="w3_footer_grid_list1">
+                            <li><label class="fa fa-twitter" aria-hidden="true"></label><i>01 day ago</i><span>Non numquam
+                                    <a href="#">http://sd.ds/13jklf#</a>
+                                    eius modi tempora incidunt ut labore et
+                                    <a href="#">http://sd.ds/1389kjklf#</a>quo nulla.</span></li>
+                            <li><label class="fa fa-twitter" aria-hidden="true"></label><i>02 day ago</i><span>Con numquam
+                                    <a href="#">http://fd.uf/56hfg#</a>
+                                    eius modi tempora incidunt ut labore et
+                                    <a href="#">http://fd.uf/56hfg#</a>quo nulla.</span></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row agile_footer_grids">
+                    <div class="col-lg-3 col-sm-6 w3_footer_grid agile_footer_grids_w3_footer">
+                        <div class="w3_footer_grid_bottom">
+                            <h4>100% secure payments</h4>
+                            <img src="/images/card.png" alt=" " class="img-fluid" />
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 w3_footer_grid agile_footer_grids_w3_footer">
+                        <div class="w3_footer_grid_bottom">
+                            <h5>connect with us</h5>
+                            <ul class="agileits_social_icons">
+                                <li><a href="#" class="facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                <li><a href="#" class="twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                <li><a href="#" class="google"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                                <li><a href="#" class="instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                </li>
+                                <li><a href="#" class="dribbble"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="wthree_footer_copy">
+                    <p>© 2021 Corner Store. All rights reserved | Design by <a href="http://w3layouts.com/">W3layouts</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MyFooter",
+}
+</script>
+<style scoped></style>
---
+++ src/components/includes/MyHeader.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="my-header">
+        <div class="agileits_header" id="home">
+            <div class="w3l_offers">
+                <a href="products.html">Today's special Offers !</a>
+            </div>
+            <div class="w3l_search">
+                <form action="#" method="GET" class="d-flex">
+                    <input type="search" placeholder="Search.." name="search" required="required" autofocus="">
+                    <button type="submit"><span class="fa fa-search"></span></button>
+
+                </form>
+            </div>
+            <div class="w3l_header_right w3-two-three-grids">
+                <div class="product_list_header mx-lg-4">
+                    <form action="#" method="post" class="last">
+                        <fieldset>
+                            <input type="hidden" name="cmd" value="_cart" />
+                            <input type="hidden" name="display" value="1" />
+                            <input type="submit" name="submit" value="View your cart" class="button" />
+                        </fieldset>
+                    </form>
+                </div>
+                <ul>
+                    <li class="dropdown profile_details_drop">
+                        <a href="#" class="dropdown-toggle text-center" data-toggle="dropdown"><i class="fa fa-user mr-2"
+                                aria-hidden="true"></i><span class="fa fa-angle-down"></span></a>
+                        <div class="mega-dropdown-menu">
+                            <div class="w3ls_vegetables">
+                                <ul class="dropdown-menu drp-mnu">
+                                    <li><a href="login.html">Login</a></li>
+                                    <li><a href="login.html">Sign Up</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                <div class="w3l_header_right1 contact-bantbtn ml-lg-4">
+                    <h2><a href="mail.html">Contact Us</a></h2>
+                </div>
+            </div>
+        </div>
+        <div class="logo_products">
+            <header id="site-header">
+                <section class="w3l-header-4 sec-part-4">
+                    <div class="container">
+                        <nav class="navbar navbar-expand-lg navbar-light p-lg-0">
+                            <h1> <a class="navbar-brand" href="index.html">
+                                    <span class="capw3">C</span>orner <span class="subcap">Store</span>
+                                </a></h1>
+                            <ul class="navbar-nav mx-lg-auto mt-lg-0 mt-3">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="events.html">Events</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="about.html">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="products.html">Best Deals</a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href="services.html">Services</a>
+                                </li>
+
+
+                            </ul>
+                            <ul class="navbar-nav search-right mt-lg-0 mt-2 w3ls_logo_products_left1 mr-lg-3">
+                                <ul class="phone_email">
+                                    <li><a href="tel:(+0123)234-547" class=""></a><i class="fa fa-phone"
+                                            aria-hidden="true"></i>
+                                        (+0123) 234 567</li>
+                                    <li><a href="mailto:store@corner.com"><i class="fa fa-envelope-o"
+                                                aria-hidden="true"></i> store@corner.com</a></li>
+                                </ul>
+                            </ul>
+
+                            <!-- //toggle switch for light and dark theme -->
+
+                            <!-- toggle switch for light and dark theme -->
+                            <div class="mobile-position">
+                                <nav class="navigation">
+                                    <div class="theme-switch-wrapper">
+                                        <label class="theme-switch" for="checkbox">
+                                            <input type="checkbox" id="checkbox">
+                                            <div class="mode-container">
+                                                <i class="gg-sun"></i>
+                                                <i class="gg-moon"></i>
+                                            </div>
+                                        </label>
+                                    </div>
+                                </nav>
+                            </div>
+                        </nav>
+                    </div>
+                </section>
+            </header>
+            <!--//header-->
+        </div>
+        <!--//navbar-->
+        
+
+        <!--//end-navbar-->
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MyHeader",
+}
+</script>
+<style scoped></style>
