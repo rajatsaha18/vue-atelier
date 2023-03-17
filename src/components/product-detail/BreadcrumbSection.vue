0a217c121e76962c9857e6c6f0666640bbff3e11
rajatsaha18 <editorrajatdiu@gmail.com>
2023-03-17 10:47:05 +0600

    product detail divide into componenet

---
+++ src/components/product-detail/BreadcrumbSection.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="bread-crumb">
+        <div class="products-breadcrumb">
+			<div class="container">
+				<ul>
+					<li><i class="fa fa-home" aria-hidden="true"></i><router-link
+							:to="{ name: 'home' }">Home</router-link><span>|</span></li>
+					<li>Single Page</li>
+				</ul>
+			</div>
+		</div>
+    </div>
+</template>
+
+<script>
+export default{
+    name:"BreadcrumbSection",
+}
+</script>
+
+<style scoped></style>
